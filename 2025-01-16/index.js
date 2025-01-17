30faf1f0ce51540fc4a23d359c7de757adf3ad85
martynass1985 <marat19@gmail.com>
2025-01-17 08:57:56 +0200

    prideta

--- 2025-01-16/index.js
+++ 2025-01-16/index.js
@@ -63,7 +63,8 @@ console.log("3. Suskačiuoti trejetus", countAllThrees(skaiciai));
 // Pirmoji funkcija suskaičiuoja, kiek yra lyginių skaičių pateiktame masyve.
 //  Kita suskaičiuoja kiek yra nelyginių skaičių;
 
-const numberArray = [1, 5, 3, 8, 10, 15, 3, 9, 4, 3, 2];
+const numberArray = [1, 5, 3, 8, 10, 2, 15, 3, 9, 4, 3, 6, 2];
+
 function countAllOdd(array) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 == 0 || typeof array[i] !== "number") {
@@ -73,6 +74,7 @@ function countAllOdd(array) {
   }
   return array.length;
 }
+
 function countAllEven(array) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 > 0 || typeof array[i] !== "number") {
@@ -82,5 +84,82 @@ function countAllEven(array) {
   }
   return array.length;
 }
-console.log("4. Suskaičiuoti visus nelyginius", countAllOdd(numberArray));
-console.log("4. Suskaičiuoti visus lyginius", countAllEven(numberArray));
+// console.log("4. Suskaičiuoti visus nelyginius", countAllOdd(numberArray));
+// console.log("4. Suskaičiuoti visus lyginius", countAllEven(numberArray));
+
+// 5. Sukurkite funkciją countAllEvenUnderSix(array), kuri suskaičiuoja, kiek yra lyginių skaičių,
+// kurių reikšmė yra  6 arba mažesnė  pateiktame masyve.
+
+function countAllEvenUnderSix(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 > 0 || array[i] > 6) {
+      array.splice(i, 1);
+      i--;
+    }
+  }
+  return array.length;
+}
+
+// console.log(
+//   "5. Paskaičiuoti lyginius iki 6",
+//   countAllEvenUnderSix(numberArray)
+// );
+
+// 6.Sukurkite funkciją throwCoin(), kuri imituoja monetos metimą.
+// funkcijos gražinama reikšmė: “H” - Herbas arba “S” - Skaičius;
+
+function throwCoin() {
+  let ismesta = rand(0, 1);
+  if (ismesta === 0) return "H";
+  return "S";
+}
+console.log(throwCoin());
+
+// Sukurkite funkciją, kuri imituoja monetos metimą 20 kartų,
+// suskaičiuokite kiek kartų iškrito skaičius, kiek kartų iškrito herbas.
+//  Paskaičiuokite procentaliai, kokiu dažnumu krito skaičius, kokiu dažnumu krito herbas;
+
+// function countH(mas) {
+//   for (let i = 0; i < mas.length; i++) {
+//     let herbai = [];
+//     if (mas[i] == "H") {
+//       herbai.push(mas[i]);
+//     }
+//   }
+//   return herbai.length;
+// }
+
+// function countS(masy) {
+//   for (let i = 0; i < masy.length; i++) {
+//     let skaiciai = [];
+//     if (masy[i] == "S") {
+//       skaiciai.push(masy[i]);
+//     }
+//   }
+//   return masy.length;
+// }
+
+function throwCointwentyTimes() {
+  let iskritoHerbas = 0;
+  let iskritoSkaicius = 0;
+  for (let i = 0; i < 20; i++) {
+    let iskrito = throwCoin();
+    if (iskrito === "S") iskritoSkaicius++;
+    else iskritoHerbas++;
+  }
+  let iskritoSkaiciusProc = ((iskritoSkaicius / 20) * 100).toFixed(0);
+  let iskritoHerbasProc = ((iskritoHerbas / 20) * 100).toFixed(0);
+  return (
+    "Skaičius iškrito " +
+    iskritoSkaicius +
+    " kartų, tai yra - " +
+    iskritoSkaiciusProc +
+    "% nuo mestų kartų skaičiaus, o herbas iškrito " +
+    iskritoHerbas +
+    " kartų, tai yra - " +
+    iskritoHerbasProc +
+    "% nuo mestų kartų skaičiaus."
+  );
+}
+
+console.log(throwCointwentyTimes());
