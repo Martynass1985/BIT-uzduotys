3f64d655148017a9c696ae6329da089a4dcafbeb
martynass1985 <marat19@gmail.com>
2025-01-18 22:18:32 +0200

    pabaiga

--- 2025-01-17/index.js
+++ 2025-01-17/index.js
@@ -165,3 +165,20 @@ function armstrongNumbers() {
 }
 
 armstrongNumbers();
+
+// 9. Parašykite funkcija kuri paskaičiuotų atstumą tarp dviejų taškų
+// koordinačių sistemoje. Parametrai 4 perduodami skaičiai x1, y1, x2, y2
+// @example:
+// (0,0) (0,1) => 1
+// (0,0) (1,0) => 1
+// (-5,0) (10,-10) =>
+// 18.027756377319946
+
+function distanceBetween(x1, y1, x2, y2) {
+  let a = x1 - x2;
+  let b = y1 - y2;
+  let c = Math.hypot(a, b);
+  return c;
+}
+
+console.log("9. Atstumas Tarp taškų", distanceBetween(-5, 0, 10, -10));
